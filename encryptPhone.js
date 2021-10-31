94d4ca92c9d03e57cf3469726afdc3d515440e19
Raul Veras <verasraul@gmail.com>
2021-10-31 00:42:32 -0400

    updating files

--- encryptPhone.js
+++ encryptPhone.js
@@ -5,4 +5,22 @@
  */
 
 
-module.exports = encryptPhoneNumber;
+
+  function encryptPhoneNumber(number) {
+    //Holds our new encrypted string
+    let encrypted = "";
+    //Loops through the string
+    for (let i = 0; i < number.length; i++){
+      //Checks if i is < 7 to get the first 6 digits
+      //Also checks to make sure the - is not turned into *
+      //Adds to encrypted
+      (i < 7 && number[i] != "-") ? encrypted += "*" : encrypted += number[i];
+    }
+    //Return our new string
+    return encrypted;
+  };
+
+
+//   module.exports = encryptPhoneNumber; 
+export default encryptPhoneNumber;
+console.log(encryptPhoneNumber("718-987-6543"));
